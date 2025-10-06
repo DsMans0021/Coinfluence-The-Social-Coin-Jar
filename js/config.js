90b3ab43a62377efdc2acf72ad040edaf2289ce3
محمد عبدالعزيز عبدالعزيز المليجي <136956569+DsMans0021@users.noreply.github.com>
2025-10-06 20:31:51 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,6 @@
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCS3VrMBmUE2-sjfHLd445HkgLHKvmFiu4",
-  authDomain: "coin-81c18.firebaseapp.com",
-  databaseURL: "https://coin-81c18-default-rtdb.firebaseio.com",
-  projectId: "coin-81c18",
-  storageBucket: "coin-81c18.firebasestorage.app",
-  messagingSenderId: "558043083775",
-  appId: "1:558043083775:web:d0512fdf66a00d1f1cadca",
-  measurementId: "G-E00B895D96"
+  FIRE_AUTH_TOKEN
 };
 
 // Initialize Firebase
